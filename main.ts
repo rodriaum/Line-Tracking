a147af1c8b0db34c2ec876ad7d31169d03e800cb
Rodrigo Ferreira <119365213+rodriaum@users.noreply.github.com>
2024-03-12 22:40:38 +0000

    Removido funções estéticas que causam delay ao detectar o Patrol.

--- main.ts
+++ main.ts
@@ -7,6 +7,10 @@
 
 let blackBackground = false, activate = false;
 
+basic.showIcon(IconNames.Happy);
+
+/** Eventos dos Botões */
+
 // Ao clicar no botão 'A' o sistema inicia ou termina se já iniciado.
 input.onButtonPressed(Button.A, () => activate = !activate);
 // Ao clicar no botão 'B' ele muda o sistema que detecta as linhas seja preto ou branco.
@@ -16,7 +20,7 @@ input.onButtonPressed(Button.B, () => blackBackground = !blackBackground);
 
 basic.forever(function () {
     if (activate) {
-    
+
     if (blackBackground) {
         if (left() == 0 && right() == 0)
             handleRunAhead();
@@ -40,33 +44,22 @@ basic.forever(function () {
 
     } else {
         maqueen.motorStop(maqueen.Motors.All);
-        basic.showIcon(IconNames.House);
     }
 })
 
 function handleRunAhead() {
-    // Lado bonito: Led e Icon.
-    basic.showIcon(IconNames.Happy);
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff);
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff);
-    // Evento do motor: Continua em Frente.
+    // Continua em Frente.
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 80);
 }
 
 function handleRunRight() {
-    // Lado bonito: Led e Icon.
-    basic.showIcon(IconNames.Snake);
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn);
-    // Evento do motor: Vira a Direita.
+    // Vira a Direita.
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 15);
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 80);
 }
 
 function handleRunLeft() {
-    // Lado bonito: Led e Icon.
-    basic.showIcon(IconNames.Snake);
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn);
-    // Evento do motor: Vira a Esquerda.
+    // Vira a Esquerda.
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 80);
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 15);
 }
